b6f4a9fe1d396c0f457ec0e85b63cd17a5a4b74a
AiRomance <WHFF521@OUTLOOK.COM>
2025-08-04 13:40:55 +0800

    更改数据类型变换

--- utils/loss.py
+++ utils/loss.py
@@ -264,7 +264,9 @@ class ComputeLoss:
             # Append
             a = t[:, -1].long()  # anchor indices 取整
             # indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
-            indices.append((b, a, gj.clamp_(0, feature_wh[1] - 1), gi.clamp_(0, feature_wh[0] - 1)))  # image, anchor, grid indices
+            gi = gi.clamp(0, feature_wh[0] - 1).long()
+            gj = gj.clamp(0, feature_wh[1] - 1).long()
+            indices.append((b, a, gj, gi))  # image, anchor, grid indices
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
